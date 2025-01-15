0ad45631b80a56f9607c23c3cb744cb93a928691
Abhishek Dam <abhishek_dam@yahoo.com>
2025-01-15 23:54:55 +0530

    categories scroll updated

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Category = () => {
   const [slide, setSlide] = useState(0);
   const [categories, setCategories] = useState([]);
+  const scrollRef = useRef(null);
 
   const nextSlide = () => {
     if (categories.length - 8 == slide) return false;
@@ -13,6 +14,24 @@ const Category = () => {
   const prevSlide = () => {
     if (slide == 0) return false;
     setSlide(slide - 6);
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: 1000,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: -1000,
+        behavior: "smooth",
+      });
+    }
   };
 
   const fetchCategory = async () => {
@@ -35,27 +54,32 @@ const Category = () => {
           <div className="flex">
             <div
               className="cursor-pointer flex items-center justify-center h-[30px] w-[30px] bg-slate-300 rounded-full mx-2"
-              onClick={() => prevSlide()}
+              onClick={scrollLeft}
             >
               <FaArrowLeft />
             </div>
             <div
               className="cursor-pointer flex items-center justify-center h-[30px] w-[30px] bg-slate-300 rounded-full mx-2"
-              onClick={() => nextSlide()}
+              onClick={scrollRight}
             >
               <FaArrowRight />
             </div>
           </div>
         </div>
-        <div className="flex overflow-hidden relative z-[-1]">
+        <div
+          // className="flex overflow-hidden relative z-[-1]"
+          ref={scrollRef}
+          className="flex overflow-x-auto gap-4 scrollbar-hide scroll-smooth px-2 w-full"
+        >
           {categories.map((cat, i) => {
             return (
               <div
-                style={{
-                  transform: `translateX(-${slide * 100}%)`,
-                }}
+                // style={{
+                //   transform: `translateX(-${slide * 100}%)`,
+                // }}
                 key={i}
-                className="w-[150px] shrink-0 duration-500"
+                // className="w-[150px] shrink-0 duration-500"
+                className="min-w-[150px] flex flex-col items-center justify-center cursor-pointer transition duration-300"
               >
                 <img
                   src={"http://localhost:5173/src/assets/images/" + cat.image}
